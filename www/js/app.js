07d434397830d7b11c312ef03eeede27af5a3985
Gerardo Grimaldi <grimaldi.gerardo@gmail.com>
2015-03-11 09:22:15 -0300

    Fix in routes

--- www/js/app.js
+++ www/js/app.js
@@ -24,63 +24,62 @@ angular.module('starter', ['ionic', 'starter.controllers','mgo-angular-wizard'])
   $stateProvider
 
   .state('app', {
-    url: "/app",
+    url: '/app',
     abstract: true,
-    templateUrl: "templates/menu.html",
+    templateUrl: 'templates/menu.html',
     controller: 'AppCtrl'
   })
 
   .state('app.login', {
-    url: "/login",
+    url: '/login',
     views: {
       'menuContent': {
-        templateUrl: "templates/login.html"
+        templateUrl: 'templates/login.html'
       }
     }
   })
 
-  .state('app.login', {
-    url: "/signup",
+  .state('app.signup', {
+    url: '/signup',
     views: {
       'menuContent': {
-        templateUrl: "templates/signup.html"
+        templateUrl: 'templates/signup.html'
       }
     }
   })
-    signup
-
+    
   .state('app.search', {
-    url: "/search",
+    url: '/search',
     views: {
       'menuContent': {
-        templateUrl: "templates/search.html"
+        templateUrl: 'templates/search.html'
       }
     }
   })
 
   .state('app.browse', {
-    url: "/browse",
+    url: '/browse',
     views: {
       'menuContent': {
-        templateUrl: "templates/browse.html"
+        templateUrl: 'templates/browse.html'
       }
     }
   })
     .state('app.playlists', {
-      url: "/playlists",
+      url: '/playlists',
       views: {
         'menuContent': {
-          templateUrl: "templates/playlists.html",
+          templateUrl: 'templates/playlists.html',
           controller: 'PlaylistsCtrl'
         }
       }
     })
 
   .state('app.single', {
-    url: "/playlists/:playlistId",
+    url: '/playlists/:playlistId',
     views: {
       'menuContent': {
-        templateUrl: "templates/playlist.html",
+        templateUrl: 'templates/playlist.html',
         controller: 'PlaylistCtrl'
       }
     }
@@ -88,5 +87,3 @@ angular.module('starter', ['ionic', 'starter.controllers','mgo-angular-wizard'])
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/login');
 });
-
-
